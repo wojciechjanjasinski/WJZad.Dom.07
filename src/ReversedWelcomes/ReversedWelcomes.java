106c897af77c9088f17c705aa6d59137486ca0dd
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-26 12:03:52 +0200

    Build a functioning program which shows ReversedWelcomes.

---
+++ src/ReversedWelcomes/ReversedWelcomes.java
@@ -0,0 +1,27 @@
+package ReversedWelcomes;
+
+import java.util.Scanner;
+
+public class ReversedWelcomes {
+    public static void main(String[] args) {
+        String[] names = new String[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Podaj proszę pięć imion jedno po drugim");
+            names[i] = scanner.nextLine();
+        }
+
+        scanner.close();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Wprowadziłeś następujące imię: " + names[i]);
+        }
+
+        for (int i = 4; i <= names.length; i--) {
+            System.out.println("Witaj " + names[i] + " !");
+            if (i == 0)
+                break;
+        }
+
+    }
+}
