1bf161c724332202218260021f1aaad3b00137aa
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-28 14:29:20 +0200

    Now program gives better feedback to the user.

--- src/GuessNumber/GuessNumber.java
+++ src/GuessNumber/GuessNumber.java
@@ -5,15 +5,31 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Zgadnij proszę liczbę:");
-        int userInputNumber = scanner.nextInt();
+        int userInputNumber;
 
-        while (!((userInputNumber > 100 && userInputNumber < 200) && userInputNumber % 3 == 0)) {
-            System.out.println("Niestety Twoja liczba albo jest spoza oczekiwanego zakresu lub nie jest podzielna przez 3 bez reszty. Spróbuj proszę jeszcze raz:");
+        do{
+            System.out.println("Zgadnij proszę liczbę:");
             userInputNumber = scanner.nextInt();
+            if (userInputNumber < 100){
+                System.out.println( "Liczba: " + userInputNumber + " podana przez Ciebie jest zbyt mała. Spróbuj proszę zgadnąć jeszcze raz.");
+                userInputNumber = scanner.nextInt();
+            }
+            if (userInputNumber > 200){
+                System.out.println( "Liczba: " + userInputNumber + " podana przez Ciebie jest zbyt duża. Spróbuj proszę zgadnąć jeszcze raz.");
+                userInputNumber = scanner.nextInt();
+            }
+            if (userInputNumber % 3 != 0){
+                System.out.println("Liczba: " + userInputNumber + " podana przez Ciebie nie dzieli się przez 3. Spróbuj proszę zgadnąć jeszcze raz.");
+                userInputNumber = scanner.nextInt();
+            }
+        }while (!((userInputNumber > 100 && userInputNumber < 200) && userInputNumber % 3 == 0));
 
-        }
-        System.out.println("Brawo! Twoja liczba: " + userInputNumber + " jest poprawna ponieważ dzieli się przez 3 i wchodzi w zakres liczb od 100 do 200");
+//        while (!((userInputNumber > 100 && userInputNumber < 200) && userInputNumber % 3 == 0)) {
+//            System.out.println("Niestety Twoja liczba albo jest spoza oczekiwanego zakresu lub nie jest podzielna przez 3 bez reszty. Spróbuj proszę jeszcze raz:");
+//            userInputNumber = scanner.nextInt();
+//
+//        }
+        System.out.println("Brawo! Twoja liczba: " + userInputNumber + " jest poprawna ponieważ dzieli się przez 3 i wchodzi w zakres liczb od 100 do 200.");
         scanner.close();
     }
 }
